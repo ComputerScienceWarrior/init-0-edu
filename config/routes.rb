52a4df4f308225c68860e65d3a2ca048f9017389
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-12 13:42:26 -0700

    Create resources for all models in the routes.rb file. Create a static homepage route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy] 
+  resources :teachers, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
+  resources :courses, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
+  resources :subjects, only: [:index]
+  resources :comments, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
+  root 'static#home'
 end
