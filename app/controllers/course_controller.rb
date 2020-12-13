d1bd9fadc3278a8f9ed146b815c0b04bfb0614e8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-13 12:00:46 -0700

    Create course_controller.rb and actions. Add bcrypt gem to Gemfile.

---
+++ app/controllers/course_controller.rb
@@ -0,0 +1,46 @@
+class CourseController < ApplicationController
+    before_action :find_student, only: [:show, :edit, :delete]
+
+    def index
+        @courses = Course.all
+    end
+
+    def show
+    end
+
+    def new
+        @course = Course.new
+    end
+
+    def create
+        @course = Course.create(course_params)
+        if @course
+            redirect_to course_path(@course)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+    private
+
+    def course_params
+        params.require(:course).permit(:title, :description, :duration, :rating, :teacher_id)
+    end
+
+    def find_student
+        @student = Student.find(params[:id])
+    end
+
+end
