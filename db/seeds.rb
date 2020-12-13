14f01e3bf305a5cdc1b34dfcadc140c150a1b3ba
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-13 13:05:49 -0700

    Create Student show and index views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 5.times do |i|
-    Student.create!(firstname: "Student-#{i}", lastname: "Lasty#{i}", username: "super-student-#{i}", email: "student_#{i}@jamesmail.com", password: "pass123")
+    Student.create(firstname: "Student-#{i}", lastname: "Lasty#{i}", username: "super-student-#{i}", email: "student_#{i}@jamesmail.com", password: "pass123")
 end
 
 2.times do |i|
-    Teacher.create!(firstname: "Teacher-#{i}", lastname: "Lasty#{i}", username: "super-teacher-#{i}", email: "teacher_#{i}@jamesmail.edu", password: "pass123")
+    Teacher.create(firstname: "Teacher-#{i}", lastname: "Lasty#{i}", username: "super-teacher-#{i}", email: "teacher_#{i}@jamesmail.edu", password: "pass123")
 end
 
 1.times do |i|
-    Course.create!(title: "course-#{i}", description: "This course is going to teach you all the basics!", teacher_id: 1)
+    Course.create(title: "course-#{i}", description: "This course is going to teach you all the basics!", teacher_id: 1)
 end
 
 2.times do |i|
