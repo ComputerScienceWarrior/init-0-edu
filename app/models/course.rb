b85c176fcea6dc49d7470486a7cb6e08e55f16be
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-14 14:00:11 -0700

    Create nested attributes in routes.rb. Update model to have accepts_nested_attributes_for

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,8 @@
 class Course < ApplicationRecord
+    has_many :topics
+    accepts_nested_attributes_for :topics
     has_many :students, through: :comments
     has_many :comments, through: :students
-    has_many :topics
     belongs_to :teacher
     validates_presence_of :title, :description, :teacher_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :students, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy] 
   resources :teachers, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
-  resources :courses, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
+  resources :courses, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy] do 
+    resources :topics, only: [:new]
+  end
   resources :topics, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
   resources :comments, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
   root 'static#home'
