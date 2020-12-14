5b58e8c8859b81806ee0ff5d9a9a8e59ccbdc0b9
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-13 17:36:41 -0700

    Migrated db to postgresql in preparation for future deployment to heroku.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_13_150018) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "edited_at"
