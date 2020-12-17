ad417c615faaaeea74116da541a7aab8d6264e81
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-17 12:34:35 -0700

    Remove staticcontroller and all references to the home.html.erb static view. Update routes.rb to reroute to new student signup page.

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
   resources :topics, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
   resources :comments, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
  
-  root 'static#home'
+  root 'students#new'
 end
