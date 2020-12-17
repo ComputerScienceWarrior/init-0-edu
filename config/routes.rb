1a131da2757647132f5f2e3e168131808af530e8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-17 14:19:33 -0700

    Add delete route to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy] 
+  resources :students, only: [:index, :show, :new, :create, :edit, :update] 
   get "/signup", to: "students#new"
   post "signup", to: "students#new"
+
+  delete '/students/:id', to: 'students#destroy'
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create", as: "sessions"
