68a5b969ec5c689dc84081d3bf728d86bdb09a7c
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-16 17:59:59 -0700

    Change sessions actions to more rails conventional names (new, create).

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
-    def new_student
+    def new
         @student = Student.new
     end
 
-    def student_login
+    def create
         @student =  Student.find_by(username: params[:username])
         if @student && @student.authenticate(params[:password])
             session[:student_id] = @student.id
             redirect_to student_path(@student)
         else
-            redirect_to '/login'
+            redirect_to login_path
         end
     end
 
