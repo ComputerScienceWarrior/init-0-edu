c7b6ef9a09ba4189ce48344752b7872aa555c049
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-18 06:59:47 -0700

    Create admin accounts using seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 
-student = Student.create!(firstname: "James", lastname: "Stampley", username: "username", email: "james@gmail.com", password: "pass123")
-course = Course.create!(title: "Data Structures and Algorithms", description: "This course is going to teach you all the basics of DS and Algorithms!", teacher_id: 1)
-topic = Topic.create!(title: "Arrays Pt.1", description: "This course will cover the S/T complexity of the Array DS.", course_id: 1)
+student = Student.create!(firstname: "James", lastname: "Stampley", username: "ADMIN-USER-1", email: "jamesryanstampley@gmail.com", password: "", is_admin: true)
 
 
