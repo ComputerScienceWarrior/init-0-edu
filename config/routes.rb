ce45f61ce6fe14d9620e74dfa7c6fa8576ffd80a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-19 07:45:52 -0700

    Organize routes in routes.rb file so I can edit them faster.

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #################### STUDENT ROUTES ####################
   resources :students, only: [:index, :show, :new, :create, :edit, :update] 
   get "/signup", to: "students#new"
   post "signup", to: "students#new"
-
   delete '/students/:id', to: 'students#destroy'
 
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create", as: "sessions"
-
-  get "/logout", to: "sessions#destroy"
-
+  #################### COURSES / NESTED TOPICS ROUTES ####################
   #nested attribute to create a topic when a new course is created
-  resources :courses, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy] do 
+  resources :courses, only: [:index, :show, :new, :create, :edit, :update] do 
     resources :topics, only: [:new]
   end
+  delete '/courses/:id', to: 'courses#destroy'
 
+  #################### TOPICS ROUTES ####################
   resources :topics, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
+
+  #################### COMMENTS ROUTES ####################
   resources :comments, only: [:index, :show, :new, :create, :edit, :update, :delete, :destroy]
+
+  #################### SESSION ROUTES ####################
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create", as: "sessions"
+  get "/logout", to: "sessions#destroy"
  
+  #################### ROOT ROUTE ####################
   root 'students#new'
+
 end
