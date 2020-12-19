5c7905c7ad793c00abdb6230cb567d7de1c60aff
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-19 14:22:12 -0700

    Update seed file to generate more students.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+students_firstname = ["James", "Kenny", "Kris"]
+students_lastname = ["Stampley", "Kay", "King"]
+students_email = ["james@gmail.com", "kenny@gmail.com", "kris@gmail.com"]
+students_username = ["jstampl12", "kennykay87", "kking13"]
 
-student = Student.create!(firstname: "James", lastname: "Stampley", username: "ADMIN-USER-1", email: "jamesryanstampley@gmail.com", password: "", is_admin: true)
+students_firstname.each.with_index(0) do |student, i|
+    student = Student.create!(firstname: students_firstname[i], lastname: students_lastname[i], username: students_username[i], email: students_email[i], password: "1", is_admin: false)
+end
 
 
