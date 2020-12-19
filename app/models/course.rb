ee713f62060bbfa54378613ab92c01af207e4cea
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-19 13:54:08 -0700

    Add new relationship 'has_many :students'

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
     has_many :topics
     accepts_nested_attributes_for :topics
+    has_many :students
     has_many :students, through: :comments
     has_many :comments, through: :students
     validates_presence_of :title, :description
