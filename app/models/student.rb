4d6011da3484a3268902c8d80ea6560681be5442
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-21 15:07:33 -0700

    App validations for student username length and to prevent spaces in username.

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,13 +6,13 @@ class Student < ApplicationRecord
     validates_presence_of :username, :email
     validates :username, uniqueness: true
     validates :email, uniqueness: true
-
+    validates :username, format: { without: /\s/, message: "cannot contain any spaces!" }
+    validates :username, presence: true, length: { minimum: 10, message: "must be at least 10 characters." }
+ 
     def self.from_omniauth(auth)
       # Creates a new student only if it doesn't exist
       where(email: auth.info.email).first_or_initialize do |student|
-        student.firstname = auth.info.name
-        student.lastname = auth.info.name
-        student.username = auth.info.name
+        student.username = auth.info.name.strip
         student.email = auth.info.email
         student.password = auth.info.name
         student.is_admin = false
