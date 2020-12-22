452c3f367ed2ca405de06ae51b08fdb96181c161
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-22 15:49:40 -0700

    Add .by_course class level method to Topic model to find a topic by it's course.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
     belongs_to :course
     validates_presence_of :title, :description
+
+    def self.by_course(course_id)
+        where(course: course_id)
+    end
 end
