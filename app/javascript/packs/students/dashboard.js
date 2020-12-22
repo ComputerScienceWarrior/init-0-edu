2f8990a58365a7fa47ca76e2b44ea2a36af295f8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-22 09:08:21 -0700

    Update table styling for all admin index views

--- app/javascript/packs/students/dashboard.js
+++ app/javascript/packs/students/dashboard.js
@@ -1,16 +1,12 @@
 document.addEventListener('turbolinks:load', ()=>{
-    const create_student_btn = document.querySelector('#create_student');
-    // create_student_btn.addEventListener('mouseover', function(){
-    //     create_student_btn.style.color = "green"
-    //     create_student_btn.style.width = "280px"
-    //     create_student_btn.style.height = "30px"
-    //     create_student_btn.style.paddingBottom = "30px"
-    // })
-    // create_student_btn.addEventListener('mouseout', function(){
-    //     create_student_btn.style.color = "black"
-    //     create_student_btn.style.width = "270px"
-    //     create_student_btn.style.height = "20px"
-    //     create_student_btn.style.fontWeight = "none"
-    // })
+    const button = document.querySelector('#view-comment-btn');
+    const courseCommentLink = document.querySelector('#course-comments-link')
+        button.addEventListener('click', function(){
+            var optVal = document.querySelector('#comment_course_id').value
+            var href = `/admin/courses/${optVal}/comments`
+            courseCommentLink.href = href
+            courseCommentLink.innerHTML = "View Comments"
+            
+        })
 })
 
