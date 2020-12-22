78118642b6c558da19c37faaf3646b8a810ac369
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-21 20:15:46 -0700

    Update font of pages. Remove JS code for testing purposes.

--- app/javascript/packs/students/dashboard.js
+++ app/javascript/packs/students/dashboard.js
@@ -1,15 +1,16 @@
 document.addEventListener('turbolinks:load', ()=>{
     const create_student_btn = document.querySelector('#create_student');
-    create_student_btn.addEventListener('mouseover', function(){
-        create_student_btn.style.color = "green"
-        create_student_btn.style.width = "270px"
-        create_student_btn.style.height = "20px"
-    })
-    create_student_btn.addEventListener('mouseout', function(){
-        create_student_btn.style.color = "black"
-        create_student_btn.style.width = "300px"
-        create_student_btn.style.height = "25px"
-        create_student_btn.style.fontWeight = "none"
-    })
+    // create_student_btn.addEventListener('mouseover', function(){
+    //     create_student_btn.style.color = "green"
+    //     create_student_btn.style.width = "280px"
+    //     create_student_btn.style.height = "30px"
+    //     create_student_btn.style.paddingBottom = "30px"
+    // })
+    // create_student_btn.addEventListener('mouseout', function(){
+    //     create_student_btn.style.color = "black"
+    //     create_student_btn.style.width = "270px"
+    //     create_student_btn.style.height = "20px"
+    //     create_student_btn.style.fontWeight = "none"
+    // })
 })
 
