d4dc4c0abbe0c68200ecabb53b59768c2a2ac595
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-22 09:12:55 -0700

    Update admin dashboard to include link to 'All Comments'

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
     belongs_to :student
     belongs_to :course
     validates_presence_of :content, :student_id, :course_id
+
+    def self.by_course(course_id)
+        where(course: course_id)
+    end
 end
