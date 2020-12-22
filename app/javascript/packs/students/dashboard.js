c7b9002b1dcab4e164c26c8961893e91d5d5fe88
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-21 19:36:04 -0700

    Update javascript and the login form.

--- app/javascript/packs/students/dashboard.js
+++ app/javascript/packs/students/dashboard.js
@@ -2,13 +2,13 @@ document.addEventListener('turbolinks:load', ()=>{
     const create_student_btn = document.querySelector('#create_student');
     create_student_btn.addEventListener('mouseover', function(){
         create_student_btn.style.color = "green"
-        create_student_btn.style.width = "220px"
-        create_student_btn.style.height = "25px"
+        create_student_btn.style.width = "270px"
+        create_student_btn.style.height = "20px"
     })
     create_student_btn.addEventListener('mouseout', function(){
         create_student_btn.style.color = "black"
-        create_student_btn.style.width = "200px"
-        create_student_btn.style.height = "20px"
+        create_student_btn.style.width = "300px"
+        create_student_btn.style.height = "25px"
         create_student_btn.style.fontWeight = "none"
     })
 })
