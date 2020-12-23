99d433cd748590e9bedefb22f2cc5e5c90ffb2a4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-22 19:56:55 -0700

    Create admin/topics_controller.rb with all CRUD actions.

---
+++ app/controllers/admin/topics_controller.rb
@@ -0,0 +1,56 @@
+class Admin::TopicsController < ApplicationController
+    before_action :find_course_and_topic, only: [:show, :edit, :update, :destroy]
+    before_action :render_dashboard, only: [:show, :edit]
+
+    def index
+        @course = Course.find(params[:course_id])
+        @topics = Topic.by_course(@course)
+        render_dashboard
+    end
+
+    def show
+    end
+
+    def new
+        @topic = Topic.new
+        render_dashboard
+    end
+
+    def create
+        @topic = Topic.create(topic_params)
+        if @topic
+            redirect_to topic_path(@topic)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @topic.update(topic_params)
+        if @topic
+            redirect_to topic_path(@topic)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @topic.destroy
+        redirect_to admin_course_path(@course)
+    end
+
+    private
+
+    def topic_params
+        params.require(:topic).permit(:title, :description, :course_id)
+    end
+
+    def find_course_and_topic
+        @topic = Topic.find(params[:id])
+        @course = @topic.course
+    end
+
+end
