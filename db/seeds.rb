9d356c5303f5c1a8bc95a90949398207c09c73ca
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-23 04:54:16 -0700

    Update seed file to create courses.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,21 @@
-students_email = ["james@gmail.com", "kenny@gmail.com", "kris@gmail.com", "sjones@gmail.com", "justinherbert10@chargers.com"]
-students_username = ["jstampl12", "kennykay87", "kking13", "starryjones52", "Jherbs10", ]
-password = 'james123'
-students_username.each.with_index(0) do |student, i|
-    student = Student.create!(username: students_username[i], email: students_email[i], password: password, is_admin: false)
-end
+# students_email = ["james@gmail.com", "kenny@gmail.com", "kris@gmail.com", "sjones@gmail.com", "justinherbert10@chargers.com"]
+# students_username = ["jstampl12", "kennykay87", "kking13", "starryjones52", "Jherbs10", ]
+# password = 'james123'
+# students_username.each.with_index(0) do |student, i|
+#     student = Student.create!(username: students_username[i], email: students_email[i], password: password, is_admin: false)
+# end
 
-#for creating an admin
+# #for creating an admin
 # student1 = Student.create!(username: "init-0", email: "james@init-0.edu", password: "james123", is_admin: true)
 
 course_titles = ["DFS / BFS", "Memoization", "Hash Dash", "C++17 Full Course", "Basic Math"]
 
-@course_titles.each._with_index(0) do |course, j|
+course_titles.each.with_index(0) do |course, j|
     course = Course.create!(title: course_titles[j], 
-                            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                                 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
                                 commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
-                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                esse cillum dolore eu fugiat nulla pariatur. ."
                             )
 end
