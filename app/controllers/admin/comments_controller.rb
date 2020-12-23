cb2f40a382302abe2acf77b2a7f520a0bdec2048
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-23 16:55:50 -0700

    add action [:new, :create, :edit, :update, :destroy] and logic for routes.

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -6,10 +6,48 @@ class Admin::CommentsController < ApplicationController
         @comments = Comment.by_student(params[:student_id])
     end
 
+    def show
+    end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.create(comment_params)
+        if @comment
+            redirect_to admin_course_path(@comment.course)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @comment.update(comment_params)
+        if @comment
+            redirect_to admin_course_path(@comment.course)
+        else
+            render :edit
+        end
+    end
+
     def destroy
         @comment = Comment.find(params[:id])
         @course = Course.find(session[:course_id])
         @comment.destroy
         redirect_to admin_course_path(@course)
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :edited_at, :student_id, :course_id)
+    end
+
+    def find_comment
+        @comment = Comment.find(params[:id])
     end
 end
