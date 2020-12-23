15854bed6b43c827e7d11b6d71408d4b2a2c8b32
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-23 09:19:23 -0700

    Remove new, edit, create, update and destroy actions from TopicsController for a regular Student user. Move views to admin/topics/ view folders.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,55 +1,15 @@
 class TopicsController < ApplicationController
     before_action :find_course_and_topic, only: [:show, :edit, :update, :destroy]
-    before_action :render_dashboard, only: [:show, :edit]
+    layout "dashboard"
 
     def index
         @topics = Topic.all
-        render_dashboard
     end
 
     def show
     end
 
-    def new
-        if current_student.is_admin
-            @topic = Topic.new
-            render_dashboard
-        else
-            redirect_to student_path(current_student)
-        end
-    end
-
-    def create
-        @topic = Topic.create(topic_params)
-        if @topic
-            redirect_to topic_path(@topic)
-        else
-            render :new
-        end
-    end
-
-    def edit
-    end
-
-    def update
-        @topic.update(topic_params)
-        if @topic
-            redirect_to topic_path(@topic)
-        else
-            render :edit
-        end
-    end
-
-    def destroy
-        @topic.destroy
-        redirect_to course_path(@course)
-    end
-
     private
-
-    def topic_params
-        params.require(:topic).permit(:title, :description, :course_id)
-    end
 
     def find_course_and_topic
         @topic = Topic.find(params[:id])
