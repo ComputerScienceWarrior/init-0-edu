da40f41acbfe4f34c04c0c6bbc5a6a1c9d21e790
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-24 10:11:40 -0700

    Update before_action and create @all_topics, @topic, @course, and @topics_by_course to properly display all topics for a normal user.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,19 +1,20 @@
 class TopicsController < ApplicationController
-    before_action :find_course_and_topic, only: [:show]
+    before_action :find_course_and_topic, only: [:index, :show]
     layout "dashboard"
 
     def index
-        @topics = Topic.by_course(params[:course_id])
+        @all_topics = Topic.all
     end
 
     def show
+        @topic = @topics_by_course.find(params[:id])
     end
 
     private
 
     def find_course_and_topic
-        @topic = Topic.find(params[:id])
-        @course = @topic.course
+        @course = Course.find(params[:course_id])
+        @topics_by_course = Topic.by_course(@course)
     end
 
 end
