243164240c31e65d3c19340b1533ceaca6e77fc1
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-25 14:10:08 -0700

    Update form to display error message if password confirmation doesn't match the password entered.

--- app/javascript/packs/students/dashboard.js
+++ app/javascript/packs/students/dashboard.js
@@ -5,6 +5,7 @@ document.addEventListener('turbolinks:load', ()=>{
     const studentPasswordConf = document.querySelector('#student_password_confirmation');
     const studentEmail = document.querySelector('#student_email');
     const spaces = document.querySelector('#spaces_label')
+    const nonMatchingPasswords = document.querySelector('#non-matching_passwords_label')
     // name="student[username]"
 
     const formData = {};
@@ -14,22 +15,20 @@ document.addEventListener('turbolinks:load', ()=>{
             formData[name] = value;
             if (formData[name].includes(' ')){
                 spaces.style.visibility = 'visible';
-                console.log("No spaces allowed!")
             }else{
                 spaces.style.visibility = 'hidden'
             }
         });
     }
 
-    for(let input of [studentPassword, studentPasswordConf]){
+    for(let input of [studentPasswordConf]){
         input.addEventListener('input', ({target}) => {
             const {name, value} = target;
             formData[name] = value;
-            if (formData[name].includes(' ')){
-                spaces.style.visibility = 'visible';
-                console.log("No spaces allowed!")
+            if (formData[name] !== studentPassword.value){
+                nonMatchingPasswords.style.visibility = 'visible';
             }else{
-                spaces.style.visibility = 'hidden'
+                nonMatchingPasswords.style.visibility = 'hidden'
             }
         });
     }
