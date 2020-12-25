2fd50a7efffd9d807f36090e1d605fb2b4255b72
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-25 12:25:51 -0700

    Create JS event code to check if spaces are present, and display a certain hidden field if a space is present.

--- app/javascript/packs/students/dashboard.js
+++ app/javascript/packs/students/dashboard.js
@@ -1,12 +1,37 @@
 document.addEventListener('turbolinks:load', ()=>{
-    const button = document.querySelector('#view-comment-btn');
-    const courseCommentLink = document.querySelector('#course-comments-link')
-        button.addEventListener('click', function(){
-            var optVal = document.querySelector('#comment_course_id').value
-            var href = `/admin/courses/${optVal}/comments`
-            courseCommentLink.href = href
-            courseCommentLink.innerHTML = "View Comments"
-            
-        })
+
+    const studentUsername = document.querySelector('#student_username');
+    const studentPassword = document.querySelector('#student_password');
+    const studentPasswordConf = document.querySelector('#student_password_confirmation');
+    const studentEmail = document.querySelector('#student_email');
+    const spaces = document.querySelector('#spaces_label')
+    // name="student[username]"
+
+    const formData = {};
+    for(let input of [studentUsername]){
+        input.addEventListener('input', ({target}) => {
+            const {name, value} = target;
+            formData[name] = value;
+            if (formData[name].includes(' ')){
+                spaces.style.visibility = 'visible';
+                console.log("No spaces allowed!")
+            }else{
+                spaces.style.visibility = 'hidden'
+            }
+        });
+    }
+
+    for(let input of [studentPassword, studentPasswordConf]){
+        input.addEventListener('input', ({target}) => {
+            const {name, value} = target;
+            formData[name] = value;
+            if (formData[name].includes(' ')){
+                spaces.style.visibility = 'visible';
+                console.log("No spaces allowed!")
+            }else{
+                spaces.style.visibility = 'hidden'
+            }
+        });
+    }
 })
 
