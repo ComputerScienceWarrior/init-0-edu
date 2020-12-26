65074ca28d3c79b6da20d8ffc92537c818f62740
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-26 03:32:28 -0700

    add relationship of has_many with new video model.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,7 @@
 class Topic < ApplicationRecord
     belongs_to :course
     validates_presence_of :title, :description, :course_id, message: "must be present to create a topic."
-    
+    has_many :videos
     def self.by_course(course_id)
         where(course: course_id)
     end
