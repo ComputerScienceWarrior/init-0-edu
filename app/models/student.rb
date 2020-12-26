a02679e80cfeecbc27077591df511709789095e0
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-12-26 01:12:07 -0700

    Update .from_omniauth method to include a predefined username that meets validation requirements.

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ApplicationRecord
     def self.from_omniauth(auth)
       # Creates a new student only if it doesn't exist
       where(email: auth.info.email).first_or_initialize do |student|
-        student.username = auth.info.name.split(" ").join
+        student.username = "brandnewuser#{Student.all.count}"
         student.email = auth.info.email
         student.password = auth.info.name
         student.is_admin = false
