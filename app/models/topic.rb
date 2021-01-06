c7c2cc2ea593554782d8febdf231fa8af7fd9c1d
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-01-06 12:16:42 -0700

    change validation from 'course_id' to instead se 'course'.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
     belongs_to :course
-    validates_presence_of :title, :description, :course_id, message: "must be present to create a topic."
+    validates_presence_of :title, :description, :course, message: "must be present to create a topic."
     has_many :videos
     def self.by_course(course_id)
         where(course: course_id)
