a07526b575b3494d1487c3bb1b4c8446ec2a5d58
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-01-12 06:57:01 -0700

    Add a by_topic class method

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,8 @@
 class Video < ApplicationRecord
     belongs_to :topic
     validates_presence_of :title, :url, :topic_id, message: "is required to create a Topic's video."
+
+    def self.by_topic(topic_id)
+        where(topic: topic_id)
+    end
 end
