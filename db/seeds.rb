9bf7ebf1ab104187607f1e382923cbb87e941441
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-01-12 07:09:12 -0700

    uncomment seed file in preparation of use

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-# students_email = ["james@gmail.com", "kenny@gmail.com", "kris@gmail.com", "sjones@gmail.com", "justinherbert10@chargers.com"]
-# students_username = ["jstampl12", "kennykay87", "kking1387", "starryjones52", "Jherbs10"]
-# password = 'james123'
-# students_username.each.with_index(0) do |student, i|
-#     student = Student.create!(username: students_username[i], email: students_email[i], password: password, is_admin: false)
-# end
+students_email = ["james@gmail.com", "kenny@gmail.com", "kris@gmail.com", "sjones@gmail.com", "justinherbert10@chargers.com"]
+students_username = ["jstampl12", "kennykay87", "kking1387", "starryjones52", "Jherbs10"]
+password = 'james123'
+students_username.each.with_index(0) do |student, i|
+    student = Student.create!(username: students_username[i], email: students_email[i], password: password, is_admin: false)
+end
 
-# #for creating an admin
-# student1 = Student.create!(username: "ADMINinit0", email: "james@init-0.edu", password: "james123", is_admin: true)
+#for creating an admin
+student1 = Student.create!(username: "ADMINinit0", email: "james@init-0.edu", password: "james123", is_admin: true)
 
-# course_titles = ["Memoization", "Javascript Algorithms", "C++"]
+course_titles = ["Memoization", "Javascript Algorithms", "C++"]
 
-# course_titles.each do |course|
-#     course = Course.create!(title: course, 
-#                             description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-#                                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-#                                 veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
-#                                 commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-#                                 esse cillum dolore eu fugiat nulla pariatur. ."
-#                             )
-# end
+course_titles.each do |course|
+    course = Course.create!(title: course, 
+                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+                                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                                esse cillum dolore eu fugiat nulla pariatur. ."
+                            )
+end
 
-# course_topics = ["Optimization", "Two Number Sum", "Hello world, C++ style"]
-# course_topics.each.with_index(0) do |course_topic, k|
-#     topic = Topic.create!(title: course_topic, description: "Here is the testing course description.", course_id: "#{k + 1}")
-# end
+course_topics = ["Optimization", "Two Number Sum", "Hello world, C++ style"]
+course_topics.each.with_index(0) do |course_topic, k|
+    topic = Topic.create!(title: course_topic, description: "Here is the testing course description.", course_id: "#{k + 1}")
+end
 
 topic_videos = ["Memoization optimization", "Two Number Sum Video", "C++ Viideo"]
 video_urls = [
